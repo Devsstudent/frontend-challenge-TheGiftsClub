d3f4ba6f4eb1ebbcf2285efe1323bbba3443263c
Devsstudent <devsstudent777@gmail.com>
2025-07-27 23:38:57 +0200

    Reworked custom badge

--- src/components/CustomBadge.tsx
+++ src/components/CustomBadge.tsx
@@ -1,3 +1,28 @@
-export default function CustomBadge({ color, width }: { color: string, width?: string }) {
-  return <div className={`w-${width || '1'} bg-${color}-600 mr-4 flex-shrink-0`} />;
+export default function CustomBadge({
+  color,
+  width,
+  height = 'full',
+}: {
+  color: string;
+  width?: string;
+  height?: string;
+}) {
+
+  const tailwindColors = {
+    blue: 'bg-blue-600',
+    red: 'bg-red-600',
+    green: 'bg-green-600',
+    yellow: 'bg-yellow-600',
+    purple: 'bg-purple-600',
+    pink: 'bg-pink-600',
+    indigo: 'bg-indigo-600',
+    gray: 'bg-gray-600',
+    orange: 'bg-orange-400',
+  };
+
+  return (
+    <div
+      className={`w-${width || "1"} ? ${tailwindColors[color]} mr-4 flex-shrink-0 h-${height}`}
+    />
+  );
 }
