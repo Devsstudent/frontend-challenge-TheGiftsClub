2aaff425b65bcdbf147b2e8f35a2a213a2c495ff
Devsstudent <devsstudent777@gmail.com>
2025-07-28 17:38:20 +0200

    Feature: Confiuration des récompenses

--- src/components/GainConfig.tsx
+++ src/components/GainConfig.tsx
@@ -3,17 +3,52 @@ import { PlusIcon } from "lucide-react";
 import { PiTicketLight } from "react-icons/pi";
 import { GiftTable } from "./GiftTable";
 import { ToogleBadge } from "./ToogleBadge";
+import { useFormContext } from "react-hook-form";
+import type { Gift } from "../types/CampaignType";
+import ConditionalTable from "./ConditionalTable";
 
 export const GainConfig = () => {
+  const { watch, setValue } = useFormContext();
+
+  const currentGifts: Gift[] = watch("configuration.gifts") || [];
+
+  const addLossGift = () => {
+    const lossGiftExists = currentGifts.some((gift) => gift.type === "LOSS");
+
+    if (!lossGiftExists) {
+      const lossGift: Gift = {
+        id: Date.now().toString(),
+        name: "Perdu",
+        type: "LOSS",
+        limit: 1,
+        initial_limit: -1,
+        icon: "",
+      };
+
+      const updatedGifts = [...currentGifts, lossGift];
+      setValue("configuration.gifts", updatedGifts);
+    }
+  };
+
+  const removeLossGift = () => {
+    const updatedGifts = currentGifts.filter((gift) => gift.type !== "LOSS");
+    setValue("configuration.gifts", updatedGifts);
+  };
+
   return (
     <div>
       <div className="flex justify-between">
         <ToogleBadge
+          defaultValue={true}
+          onChange={(checked) => {
+            if (!checked) {
+              addLossGift();
+            } else {
+              removeLossGift();
+            }
+          }}
           str="Jeux 100% Gagnant"
-          description="
-          Cochez cette option pour garantir un gain à chaque joueur. Si vous la
-          décohez, une case 'Perdu' sera automatiquement ajoutée au jeu.
-            "
+          description="Cochez cette option pour garantir un gain à chaque joueur. Si vous la décochez, une case 'Perdu' sera automatiquement ajoutée au jeu."
         />
         <div className="flex justify-end">
           <div className="flex items-center gap-2">
--- src/components/GiftTable.tsx
+++ src/components/GiftTable.tsx
@@ -10,7 +10,7 @@ import {
 import { useFormContext } from "react-hook-form";
 import { IoMdInformationCircleOutline } from "react-icons/io";
 import type { Gift, GiftType } from "../types/CampaignType";
-import { Plus } from "lucide-react";
+import { Plus, Trash2 } from "lucide-react";
 
 const Row = ({ gift }: { gift: Gift }) => {
   const getCategoryString = (category: GiftType): string => {
@@ -37,14 +37,18 @@ const Row = ({ gift }: { gift: Gift }) => {
       </TableCell>
       <TableCell>
         <div className="flex justify-between items-center">
-          <span>{gift.limit}</span>
+          {gift.initial_limit === -1 ? (
+            <span className="text-gray-400 italic">Illimité</span>
+          ) : (
+            gift.limit
+          )}
           <div>
             {/* enable ou disable = gray ou pas */}
             <span>Illimité</span>
-            <Switch />
+            <Switch checked={gift.initial_limit === -1} />
           </div>
-          <div className="absolute right-[50px] flex items-center">
-            <IoMdInformationCircleOutline className="w-5 h-5 text-gray-400" />
+          <div className="absolute right-[20px] flex items-center">
+            <Trash2 className="w-5 h-5 text-gray-600" />
           </div>
         </div>
       </TableCell>
@@ -54,10 +58,37 @@ const Row = ({ gift }: { gift: Gift }) => {
 
 export const GiftTable = () => {
   const methods = useFormContext();
-  const gifts: Gift[] = methods.getValues("configuration.gifts") || [];
+  const gifts: Gift[] = methods.watch("configuration.gifts") || [];
+
+  const getOrdinalNumber = (num: number): string => {
+    if (num === 1) return "Premier";
+    if (num === 2) return "Deuxième";
+    if (num === 3) return "Troisième";
+    if (num === 4) return "Quatrième";
+    if (num === 5) return "cinquième";
+    if (num === 6) return "Sixième";
+    if (num === 7) return "Septième";
+    if (num === 8) return "Huitième";
+    if (num === 9) return "Neuvième";
+    if (num === 10) return "Dixième";
+    return `${num}ème`;
+  };
+
+  const handleAddGift = () => {
+    const newGift: Gift = {
+      id: Date.now().toString(),
+      name: "New Element",
+      type: "EAT",
+      limit: 1,
+      initial_limit: 1,
+      icon: "",
+    };
+    const updatedGifts = [...gifts, newGift];
+    methods.setValue("configuration.gifts", updatedGifts);
+  };
 
   return (
-    <div className="p-6">
+    <div className="py-6">
       <TableContainer className="border border-gray-200 rounded-xl">
         <Table className="table-auto">
           <TableHead>
@@ -96,11 +127,14 @@ export const GiftTable = () => {
               <Row key={index} gift={gift} />
             ))}
             <TableRow>
-              <TableCell >
-                <span className="text-blue-700 text-sm flex items-center justify-start gap-2 hover:cursor-pointer caca">
+              <TableCell>
+                <span
+                  className="text-blue-700 text-sm flex items-center justify-start gap-2 cursor-pointer"
+                  onClick={handleAddGift}
+                >
                   <Plus className="w-5 h-5 mr-1 rounded-full border border-blue-700" />
                   <div className="flex items-center text-center">
-                    Ajouter un {}xeme gain
+                    Ajouter un {getOrdinalNumber(gifts.length + 1)} Gain
                   </div>
                 </span>
               </TableCell>
